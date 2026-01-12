11e6c164c6704085a357c36986be38981d6e656f
asherevans682-ops <asherevans682@gmail.com>
2026-01-12 12:06:35 -0800

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,7 @@ def voice():
     return str(resp)
 
 if __name__ == "__main__":
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
